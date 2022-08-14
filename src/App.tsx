b8518345d63ab30925979813610b77e800a3ba71
Pavel Orlovsky <paul.fofanov@gmail.com>
2022-08-14 21:52:32 +0300

    auth v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Settings from "./components/settings/Settings";
 import NotFoundPage from "./components/notFoundPage/NotFoundPage";
 import Layout from "./components/Layout";
 import Login from "./components/login/Login";
+import { RequireAuth } from "./hoc/RequireAuth";
 
 function App() {
   return (
@@ -18,7 +19,14 @@ function App() {
           <Route path="transactions" element={<Transactions />} />
           <Route path="stocks" element={<Stocks />} />
           <Route path="analysis" element={<Analysis />} />
-          <Route path="settings" element={<Settings />} />
+          <Route
+            path="settings"
+            element={
+              <RequireAuth>
+                <Settings />
+              </RequireAuth>
+            }
+          />
           <Route path="login" element={<Login />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,12 +1,38 @@
 import classNames from "classnames";
+import { useLocation, useNavigate } from "react-router-dom";
+
+interface LocationState {
+  from: {
+    pathname: string;
+  };
+}
 
 const Login: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { from } = (location.state as LocationState) || {
+    from: {
+      pathname: "/",
+    },
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    navigate(from.pathname, { replace: true });
+    console.log(from);
+  };
+
   return (
     <section className={classNames("page__login", "login")}>
       <div className="login__container">
         <div className="login__body">
           <h2 className="login__title">WELCOME</h2>
-          <form className={classNames("login__form", "form-login")}>
+          <form
+            onSubmit={handleSubmit}
+            className={classNames("login__form", "form-login")}
+          >
             <label className="form-login__item">
               <div className="form-login__title"> Username</div>
               <input className="form-login__input" type="text" />
