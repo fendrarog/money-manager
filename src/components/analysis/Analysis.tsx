3fafaad1424da3d53dd99d4a00a2828b270ec733
Pavel Orlovsky <paul.fofanov@gmail.com>
2022-08-17 00:56:15 +0300

    add bar chart v1.0

--- src/components/analysis/Analysis.tsx
+++ src/components/analysis/Analysis.tsx
@@ -1,13 +1,16 @@
 import classNames from "classnames";
+import { useState } from "react";
 import {
-  LineChart,
-  Line,
+  BarChart,
+  Bar,
+  Cell,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   Legend,
   ResponsiveContainer,
+  LabelList,
 } from "recharts";
 
 const data = [
@@ -86,35 +89,113 @@ const data = [
 ];
 
 const Analysis: React.FC = () => {
+  const [activeIndex, setIndex] = useState(-2);
+
+  const handleClick = (data: any, index: number) => {
+    setIndex(index);
+  };
+
+  const renderCustomizedLabel = (props: any) => {
+    const { x, y, width, value, index } = props;
+
+    if (index === activeIndex) {
+      return (
+        <g>
+          <text
+            x={x + width / 2}
+            y={y - 10}
+            style={{ fill: "#fff" }}
+            textAnchor="middle"
+            dominantBaseline="middle"
+          >
+            {value}
+          </text>
+        </g>
+      );
+    }
+  };
+
+  //Массив в элементе которого сумма расходов всех категорий
+  const numberData = data
+    .map((item) =>
+      Object.values(item).filter(
+        (item): item is number => typeof item === "number"
+      )
+    )
+    .map((item) => item.reduce((a, b) => a + b, 0));
+
+  //Массив с объектами {_имя, сумма всех расходов за период _имя}
+  const newData = data.map((item, index) => ({
+    name: item.name,
+    pv: numberData[index],
+  }));
+
+  console.log(newData);
   return (
     <section className={classNames("page__home", "home")}>
       <div className="home__container">
         <div className="home__body">
-          <ResponsiveContainer width="100%" aspect={3}>
-            <LineChart
+          <ResponsiveContainer width="100%" aspect={2}>
+            <BarChart
               width={500}
               height={300}
-              data={data}
+              data={newData}
               margin={{
-                top: 5,
+                top: 20,
                 right: 30,
                 left: 20,
                 bottom: 5,
               }}
             >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="pv"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }}
+              <CartesianGrid vertical={false} opacity={0.1} />
+              <XAxis dataKey="name" axisLine={false} tickLine={false} />
+              <YAxis axisLine={false} tickLine={false} tickCount={8} />
+              <Tooltip
+                contentStyle={{ color: "aqua", backgroundColor: "green" }}
+                cursor={{
+                  fill: "transparent",
+                }}
+                /* labelStyle={{ color: "red", backgroundColor: "aqua" }}
+                wrapperStyle={{ color: "red", border: "5px solid gold" }}
+                itemStyle={{
+                  color: "gold",
+                  backgroundColor: "red",
+                  border: "1px solid green",
+                }} */
+                /* allowEscapeViewBox={{ x: false, y: false }}
+                position={{ x: 100, y: 40 }}
+                isAnimationActive={true}
+                animationDuration={1500} */
               />
-              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
+              <Legend />
+              <Bar
+                onClick={handleClick}
+                dataKey="pv"
+                stackId="a"
+                fill="#ffb6c1"
+              >
+                <LabelList
+                  dataKey="pv"
+                  position="top"
+                  style={{ fill: "#ffb6c1" }}
+                  offset={10}
+                  content={renderCustomizedLabel}
+                />
+                {newData.map((entry, index) => (
+                  <Cell
+                    cursor="pointer"
+                    fill={
+                      activeIndex === -2
+                        ? "#ffb6c1"
+                        : index === activeIndex
+                        ? "#8884d8"
+                        : "#ffb6c1"
+                    }
+                    key={`cell-${index}`}
+                  />
+                ))}
+              </Bar>
+            </BarChart>
           </ResponsiveContainer>
         </div>
       </div>
